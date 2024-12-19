6400c46ee474230628aa425ea6e015a2a8e28054
Niewiaro <kubuskrol@gmail.com>
2024-12-19 17:13:19 +0100

    sheets

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from openpyxl import load_workbook
+
+
+class Config:
+    def __init__(self) -> None:
+        from pathlib import Path
+
+        self.input_name = "Profile 2025 GD.xlsx"
+        self.input_path = Path.cwd() / self.input_name
+
+
 def main() -> None:
-    print("Main")
+    config = Config()
+    workbook = load_workbook(config.input_path)
+    sheets_names = workbook.sheetnames
+
+    for sheet in sheets_names:
+        print(sheet)
+
 
 if __name__ == "__main__":
     main()
-    
